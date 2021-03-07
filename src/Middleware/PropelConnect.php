9f498c51f0010decdad3ab1efffd7fc3c9e843e4
Iulian N <iulian@spartanphp.com>
2021-03-07 20:22:31 +0200

    new: propel connect middleware class

---
+++ src/Middleware/PropelConnect.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Spartan\Db\Middleware;
+
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Spartan\Db\Adapter\Propel\Propel2;
+
+/**
+ * PropelConnect Middleware
+ *
+ * @package Spartan\Db
+ * @author  Iulian N. <iulian@spartanphp.com>
+ * @license LICENSE MIT
+ */
+class PropelConnect implements MiddlewareInterface
+{
+    /**
+     * @param ServerRequestInterface  $request
+     * @param RequestHandlerInterface $handler
+     *
+     * @return ResponseInterface
+     * @throws \Exception
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        Propel2::connect();
+
+        return $handler->handle($request);
+    }
+}
